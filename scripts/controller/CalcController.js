adb6da1859a1e9c88ede47856279c15635262e41
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-21 16:48:28 -0300

    fix: correct last number display issue and update calc method to properly handle results

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -84,11 +84,24 @@ class CalcController {
         let result = eval(this._operation.join(""));
 
         this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
     }
 
     setLastNumberToDisplay(){
 
-        
+        let lastNumber;
+
+        for (let i = this._operation.length-1; i >= 0; i--){
+
+            if (!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+
+        }
+
+        this.displayCalc = lastNumber;
 
     }
 
@@ -108,6 +121,8 @@ class CalcController {
                 
                 this.pushOperation(value);
 
+                this.setLastNumberToDisplay();
+
             }
             
         } else {
@@ -121,7 +136,7 @@ class CalcController {
             let newValue = this.getLastOperation().toString() + value.toString();
             this.setLastOperation(parseInt(newValue));
             
-            this.setLastNumberToDisplay()
+            this.setLastNumberToDisplay();
 
             }
 
