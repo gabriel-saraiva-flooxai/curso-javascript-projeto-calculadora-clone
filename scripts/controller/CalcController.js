51b6df74cf090502aa5775016eea6d88c693c475
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-21 18:23:51 -0300

    feat: add keyboard support

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -13,6 +13,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
 
     }
 
@@ -30,13 +31,60 @@ class CalcController {
 
     }
 
-        addEventListenerAll(element, events, fn) {
+    initKeyboard(){
 
-            events.split(' ').forEach(event => {
+        document.addEventListener('keyup', e=>{
+            
+            switch (e.key) {
 
-                element.addEventListener(event, fn, false);
+              case "Escape":
+                this.clearAll();
+                break;
+              case "Backspace":
+                this.clearEntry();
+                break;
+              case "+":
+              case '-':
+              case '*':
+              case '/':
+              case '%':
+                this.addOperation(e.key);
+                break;
+              case ".":
+              case ",":
+                this.addDot(".");
+                break;
+              case "Enter":
+              case "=":
+                this.calc();
+                break;
 
-            });
+              case "0":
+              case "1":
+              case "2":
+              case "3":
+              case "4":
+              case "5":
+              case "6":
+              case "7":
+              case "8":
+              case "9":
+                this.addOperation(parseInt(e.key));
+                break;
+                
+            }
+            
+        });
+
+    }
+
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        });
 
     }
 
